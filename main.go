2a1db9ac41e0937e99d531578d59b4d15e99592f
Christoph Hummer <christophhummer@gmail.com>
2024-03-18 22:52:22 +0100

    Update main.go
    - Add prefix to log output
    - Add time and date to log output
    - Reformatted log strings

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net"
 	"os"
 	"strconv"
+	"time"
 )
 
 var (
@@ -34,24 +35,26 @@ func main() {
 	} else {
 		serverAddr = addr
 	}
+	
+	currentTime := time.Now().Format("2006/01/02 15:04:05")
 
 	server := NewServer()
 	server.EnableUDP()
 	server.OnStarted(func(listener *net.TCPListener) {
-		fmt.Println("start server on", listener.Addr().String())
+		fmt.Printf("MAIN    : %v: INFO     SOCKS5 Proxy started on port: %s\n", currentTime, listener.Addr().String())
 	})
 	server.OnConnected(func(network, address string, port int) {
-		fmt.Println("["+network+"]connect to:", address+":"+strconv.Itoa(port))
+		fmt.Printf("SOCKS5  : %v: INFO     [%s] connect to: %s:%d\n", currentTime, network, address, port)
 	})
 	if username != "" || password != "" {
 		server.SetAuthHandle(handlerAuth)
 	}
 	if err := server.Run(serverAddr); err != nil {
-		fmt.Println("Run socks5 server error:", err.Error())
+		fmt.Println("MAIN    : %v: ERROR    Running SOCKS5 Proxy server error: %s\n", currentTime, err.Error())
 		os.Exit(1)
 	}
 
-	fmt.Println("Socks5 server normal exit.")
+	fmt.Println("MAIN    : %v: INFO     SOCKS5 server normal exit.\n", currentTime)
 	os.Exit(0)
 }
 
